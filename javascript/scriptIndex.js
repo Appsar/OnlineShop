9064fbfc50aea1bef7b570b063bac9ef78ecef8b
SamuelLindberg <sssamuel422@gmail.com>
2026-03-24 17:38:53 +0100

    first commit

---
+++ javascript/scriptIndex.js
@@ -0,0 +1,46 @@
+let containers = document.querySelectorAll(".flex-item");
+let currentIndex = 0;
+
+containers.forEach((container) => {
+    const img = document.createElement("img");
+    img.style.objectFit = "contain";
+    img.style.width = "100%";
+    img.style.height = "85%";
+
+    const title = document.createElement("p");
+    title.classList.add("product-title");
+    title.style.fontSize = "2rem";
+    title.style.color = "white";
+    title.style.marginBottom = "1rem"
+
+    const price = document.createElement("p");
+    price.classList.add("product-price");
+    price.style.fontSize = "2rem";
+    price.style.color = "white";
+
+    container.append(img, title, price);
+});
+
+function loadSlide(index) {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then((data) => {
+            containers.forEach((container, i) => {
+                container.querySelector("img").src = data[index + i].image
+                container.querySelector(".product-title").textContent = data[index + i].title;
+                container.querySelector(".product-price").textContent = data[index + i].price + " $";
+            });
+        });
+}
+
+loadSlide(0);
+
+document.querySelector(".next-btn").addEventListener("click", () => {
+    currentIndex = (currentIndex + 4) % 20;
+    loadSlide(currentIndex)
+})
+
+document.querySelector(".previous-btn").addEventListener("click", () => {
+    currentIndex = (currentIndex - 4 + 20) % 20;
+    loadSlide(currentIndex)
+})
